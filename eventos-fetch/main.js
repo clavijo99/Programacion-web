c89d93770dabb8624ee0620d6671d37f4da7d94b
clavijo99 <bclavijogomez@gmail.com>
2025-05-09 06:50:25 -0500

    upload example events

---
+++ eventos-fetch/main.js
@@ -0,0 +1,69 @@
+
+
+// creamos una funcion para mostrar una alerta cuando el usuario de click en el boton
+// evento de click
+function saludar() {
+    alert("Bienvenido")
+}
+
+
+const click = document.getElementById("parrafo")
+const body = document.getElementsByTagName("body")
+
+click.style.cursor = "pointer";
+
+
+// callback
+click.addEventListener("click", () => {
+    document.body.style.background = "red";
+});
+
+
+// formulario
+// obtenemos el formulario por el id
+const formulario = document.getElementById("formulario");
+// 1. document.getElemetById("")
+
+const text = document.getElementById("texto");
+
+// agregamos un evento al formulario " submit "
+formulario.addEventListener("submit", (event) => {
+    // detenemos el comportamiento que tienen los formularios de recargar la pagina al enviarlo
+    event.preventDefault()
+    console.log(event)
+    // obtenemos el valor que ingreso el usuario en el input
+    const name = event.target.nombre.value;
+
+    const apellido = event.target.apellido.value;
+
+    const edad = event.target.edad.value;
+
+    text.innerHTML = `<p>${name} - ${apellido} - ${edad} años</p>`;
+    alert(`nombre: ${name} - apellido: ${apellido} - ${edad} años`)
+})
+
+
+// promesas promise
+fetch("https://jsonplaceholder.typicode.com/users").then(
+    (response) => {
+        // esperar la respusta del servicio y la vamos a procesar
+        console.log(response)
+        return response.json();
+    }
+).then(
+    (data) => {
+        console.log(data)
+    }
+);
+
+// async - await
+
+async function getUSers() {
+    const respose = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const dataJson = await respose.json();
+    
+    console.log(dataJson)
+}
+
+getUSers();
+
